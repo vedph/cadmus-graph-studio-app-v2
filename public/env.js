cbea92ae60ae3e289fd2bb127a3a048f54ad5a0b
Myrmex <dfusi@hotmail.com>
2025-07-09 22:49:29 +0200

    More sample data

--- public/env.js
+++ public/env.js
@@ -5,5 +5,5 @@
   // environment-dependent settings
   window.__env.apiUrl = "http://localhost:5257/api/";
   window.__env.version = "0.1.0";
-  window.__env.presetMappings = 'test-mappings.json';
+  window.__env.presetMappings = 'person-mappings.json';
 })(this);
